e150a3935679f648cf86252972aa1e8ee571e501
Sidak Dhillon <dhillonsidakgt@gmail.com>
2021-02-25 18:23:52 -0500

    Added run, jump, idle states to zombie controller

--- Assets/_Scripts/ZombieBehavior.cs
+++ Assets/_Scripts/ZombieBehavior.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator
+
+    private NavMeshAgent agent;
+
     void Start()
     {
-        
+		animator = GetComponent<Animator>();
+		agent = GetComponent<NavMeshAgent>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+    	if(Input.GetKeyDown(KeyCode.I))
+    	{
+    		animator.SetInteger("AnimState", (int)ZombieState.IDLE);
+    	}
+
+    	if(Input.GetKeyDown(KeyCode.U))
+    	{
+    		animator.SetInteger("AnimState", (int)ZombieState.RUN);
+    	}
+
+    	if(Input.GetKeyDown(KeyCode.J))
+    	{
+    		animator.SetInteger("AnimState", (int)ZombieState.JUMP);
+    	}
+
         
     }
 }
